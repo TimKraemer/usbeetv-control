f2d3f695d3cd2842531e1cf296501a0eed22cab6
Tim Krämer <github@tk22.de>
2024-12-27 00:13:00 +0100

    Enhance Next.js configuration by enabling standalone output and disabling telemetry. This update improves build performance and reduces unnecessary data collection.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  env: {
+    NEXT_TELEMETRY_DISABLED: 1,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
