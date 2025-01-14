68c0b77d4197d8bc9dc4f5a54d5cd89dcff4c518
Tim Krämer <github@tk22.de>
2025-01-14 03:42:03 +0100

    Refactor movie and TV show existence checks in useExistsInDb.js. Updated state management to include 'isComplete' for movies and adjusted timeout duration from 2000ms to 1500ms for both movie and TV show checks. Removed commented-out logging for TV shows to clean up the code. This change improves the accuracy of existence checks and enhances performance.

--- src/app/lib/useExistsInDb.js
+++ src/app/lib/useExistsInDb.js
@@ -10,11 +10,11 @@ export const useMovieExistsInDb = (movie) => {
             try {
                 const response = await fetch(`/api/library/movies?tmdbId=${movie.id}`)
                 const data = await response.json()
-                setMovieExistsInDb({ exists: data.exists })
+                setMovieExistsInDb({ exists: data.exists, isComplete: data.exists })
             } catch (error) {
                 console.error("Error checking if movie exists in DB:", error)
             }
-        }, 2000)
+        }, 1500)
 
         return () => clearTimeout(timer)
     }, [movie])
@@ -30,14 +30,14 @@ export const useTvShowExistsInDb = (tvShow) => {
             try {
                 const response = await fetch(`/api/library/tvshows?tmdbId=${tvShow.id}`)
                 const data = await response.json()
-                if (data.exists) {
-                    console.log(data)
-                }
+                // if (data.exists) {
+                //     console.log(data)
+                // }
                 setTvShowStatus({ exists: data.exists, isComplete: data.missingSeasons?.length === 0 })
             } catch (error) {
                 console.error("Error checking if TV show exists in DB:", error)
             }
-        }, 2000)
+        }, 1500)
 
         return () => clearTimeout(timer)
     }, [tvShow])
